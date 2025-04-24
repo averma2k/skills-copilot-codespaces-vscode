2284cfbc7b4c61dc5304cda770374e5f54a85649
Amit Verma <amit.verma@gov.bc.ca>
2025-04-24 10:47:10 -0700

    Copilot first commit

---
+++ skills.js
@@ -0,0 +1,8 @@
+// function to covert Celsius
+// to Fahrenheit
+function celsiusToFahrenheit(celsius) { 
+    return (celsius * 9/5) + 32;
+}
+// driver code
+let celsius = 25;   
+console.log(celsius + "°C is equal to " + celsiusToFahrenheit(celsius) + "°F");
